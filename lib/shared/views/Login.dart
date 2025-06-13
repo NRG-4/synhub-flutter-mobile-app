5623e918d46acc561b5a94b64379fc14fdca9d97
Al3xiel <gabriel280104casas@gmail.com>
2025-06-13 00:42:22 -0500

    feat(login): add login screen and update main entry point

---
+++ lib/shared/views/Login.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 20),
+            const Text(
+              'SynHub',
+              style: TextStyle(
+                fontSize: 70,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF1A4E85),
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 200,
+              child: Image.asset(
+                'images/synhub_logo.png', // Asegúrate de tener la imagen en assets
+                fit: BoxFit.contain,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 20,
+                color: Color(0xFF000000),
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Insert User',
+                hintText: 'User',
+                prefixIcon: Icon(Icons.person, color: Colors.grey),
+                filled: true,
+                fillColor: Color(0xFFF3F3F3),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Insert Password',
+                hintText: 'password',
+                prefixIcon: Icon(Icons.lock, color: Colors.grey),
+                filled: true,
+                fillColor: Color(0xFFF3F3F3),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(const Color(0xFF4A90E2)),
+                  shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                onPressed: () {}, // Cambiado para habilitar el botón
+                child: const Text(
+                  'Iniciar Sesion',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(const Color(0xFFFFFFFF)),
+                  shape: WidgetStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+                onPressed: () {}, // Cambiado para habilitar el botón
+                child: const Text(
+                  'Registrase',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
