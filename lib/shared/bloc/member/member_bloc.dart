46671d483e538a670931b29fceabe12c6ed46a33
Al3xiel <gabriel280104casas@gmail.com>
2025-06-15 18:44:59 -0500

    feat(member): add LoadMemberGroupEvent and handling for member group loading

--- lib/shared/bloc/member/member_bloc.dart
+++ lib/shared/bloc/member/member_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:bloc/bloc.dart';
+import '../../../group/models/group.dart';
 import '../../models/member.dart';
 import '../../services/member_service.dart';
 import 'member_event.dart';
@@ -10,6 +11,7 @@ class MemberBloc extends Bloc<MemberEvent, MemberState> {
 
   MemberBloc({required this.memberService}) : super(MemberInitial()) {
     on<FetchMemberDetailsEvent>(_onFetchMemberDetails);
+    on<LoadMemberGroupEvent>(_onLoadMemberGroup);
   }
 
   Future<void> _onFetchMemberDetails(
@@ -28,6 +30,27 @@ class MemberBloc extends Bloc<MemberEvent, MemberState> {
       }
     } catch (e) {
       emit(MemberError('Error: $e'));
+    }
+  }
+
+  Future<void> _onLoadMemberGroup(
+      LoadMemberGroupEvent event,
+      Emitter<MemberState> emit,
+      ) async {
+    emit(MemberLoading());
+    try {
+      final response = await memberService.getMemberGroup();
+
+      if (response.statusCode == 200) {
+        final group = Group.fromJson(json.decode(response.body));
+        emit(MemberGroupLoaded(group));
+      } else if (response.statusCode == 404) {
+        emit(MemberNoGroup()); // Nuevo estado para cuando el miembro no tiene grupo
+      } else {
+        emit(MemberError('Failed to load member group'));
+      }
+    } catch (e) {
+      emit(MemberError('Error loading member group: $e'));
     }
   }
 }
