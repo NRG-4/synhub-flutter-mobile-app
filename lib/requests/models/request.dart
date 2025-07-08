2d0f849ab16732f9c74d873c2b86850921d5929f
pachieeee <sebaspacheco2003@gmail.com>
2025-07-08 00:22:47 -0500

    feat(request): add requestType field to Request model.

--- lib/requests/models/request.dart
+++ lib/requests/models/request.dart
@@ -4,12 +4,14 @@ class Request {
   final int id;
   final String description;
   final String requestStatus;
+  final String requestType;
   final Task task;
 
   Request({
     required this.id,
     required this.description,
     required this.requestStatus,
+    required this.requestType,
     required this.task
   });
 
@@ -18,6 +20,7 @@ class Request {
       id: json['id'],
       description: json['description'],
       requestStatus: json['requestStatus'],
+      requestType: json['requestType'],
       task: Task.fromJson(json['task']),
     );
   }
